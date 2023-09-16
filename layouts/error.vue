fcc3661bb46b437ec3290ecaa01a62e987a289c8
Hafidzrdwn <hafidz10032005@gmail.com>
2023-09-16 21:25:48 +0700

    [ADD] error page : 404

---
+++ layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <component :is="errorPage" :error="error" />
+  </div>
+</template>
+<script>
+  import error404 from '~/components/error/404.vue'
+  export default {
+    name: 'AppError',
+    layout: 'default',
+    props: ['error'],
+    computed: {
+      errorPage() {
+        if (this.error.statusCode === 404) {
+          return error404
+        }
+
+        return false;
+      },
+    }
+  }
+</script>
