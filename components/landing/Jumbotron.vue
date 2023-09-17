b3d105a83c1850557edde2e6a19df95b31f5a907
Hafidzrdwn <hafidz10032005@gmail.com>
2023-09-17 20:40:54 +0700

    [ADD] feature : scroll to view

--- components/landing/Jumbotron.vue
+++ components/landing/Jumbotron.vue
@@ -2,16 +2,16 @@
   <!-- JUMBOTRON -->
   <div class="jumbotron jumbotron-fluid">
     <div class="container">
-      <h1 class="text-light shadow-sm">
+      <h1 class="text-jumbotron text-light">
         the key of <span class="text-custom">happiness</span> <br />
         and <span class="text-custom">peace</span> together
       </h1>
-      <p class="lead text-light mt-3 shadow-sm">
+      <p class="text-description mt-3">
         Sayangi perutmu terlebih dahulu, singkirkan semua masalah <br />hidup
         sejenak dan berdamai bersama kami.
       </p>
       <a href="#product" class="btn btn-custom">Order sekarang</a>
-      <a class="btn btn-outline-custom ms-2" href="#about">Tentang kami</a>
+      <button type="button" class="btn btn-outline-custom ms-2" @click="handleScrollTo('about')">Tentang kami</button>
     </div>
   </div>
   <!-- END JUMBOTRON -->
@@ -19,10 +19,24 @@
 
 <script>
 export default {
-
+  name: 'Jumbotron',
+  methods: {
+    handleScrollTo(id) {
+      window.scrollTo({
+        top: document.getElementById(id).offsetTop - 5,
+        behavior: 'smooth'
+      })
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.text-jumbotron {
+  font-weight: 700;
+}
+.text-description {
+  font-size: 1.2rem;
+  color: #fff;
+}
 </style>
