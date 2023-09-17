5986d42dea655b31886f6015e038145e2eac3ef3
Hafidzrdwn <hafidz10032005@gmail.com>
2023-09-17 20:40:24 +0700

    [ADD] new component : section header

---
+++ components/SectionHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="section-header">
+      <h2 class="title">{{ title }}</h2>
+      <h4 class="subtitle">{{ subtitle }}</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      required: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.section-header {
+  margin-bottom: 40px;
+}
+
+.section-header > .title {
+  font-size: 40px;
+  color: #fff;
+  margin-bottom: 10px;
+  font-weight: 700;
+}
+.section-header > .subtitle {
+  font-size: 20px;
+  color: #42c7c1;
+}
+</style>
